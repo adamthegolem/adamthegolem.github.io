399bb327c1a2083089c3c97a587fcc6ef2a681a3
Adam <adamdamgolan@gmail.com>
2026-01-22 21:07:02 +0100

    attempt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // app: {
-  //   baseURL: "adamthegolem.github.io",
-  // },
+  app: {
+    baseURL: "adamthegolem.github.io",
+  },
   modules: [
     '@nuxt/eslint',
     '@nuxt/ui',
